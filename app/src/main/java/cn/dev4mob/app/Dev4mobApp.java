e99fc8df18a97a172f88d670c04670a43a9e7e75
dev4mobile <cn.dev4mobile@gmail.com>
2014-05-24 13:09:23 +0800

    fragment test

---
+++ app/src/main/java/cn/dev4mob/app/Dev4mobApp.java
@@ -0,0 +1,9 @@
+package cn.dev4mob.app;
+
+import android.app.Application;
+
+/**
+ * Created by dev4mobile on 5/24/14.
+ */
+public class Dev4mobApp extends Application{
+}
---
+++ app/src/main/java/cn/dev4mob/app/ui/base/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package cn.dev4mob.app.ui.base.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by dev4mobile on 5/24/14.
+ */
+public class BaseFragment extends Fragment{
+
+}
